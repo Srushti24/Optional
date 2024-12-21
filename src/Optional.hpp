91113d1cb10f3a717ee67f1eac75ea2d31fa4481
Srushti24 <srushti.desai@gmail.com>
2024-12-21 18:33:35 -0500

    added basic implementation

--- src/Optional.hpp
+++ src/Optional.hpp
@@ -1,14 +1,81 @@
+#ifndef OPTIONAL_HPP
+#define OPTIONAL_HPP
 
-template<typedef T>
+#include <iostream>
+#include <memory>
+
+template<typename T>
 class Optional{
-
     public:
-    Optional()
+    Optional():has_value_(false)
     {
+
     }
-    ~Optional();
+
+    //Param Constrcutor
+    Optional(const T& value):has_value_(true)
+    {
+        new(data_)T(value);
+    }
+
+    Optional(T&& value): has_value_(true)
+    {
+        new(data_)T(std::move(value));
+    }
+   
+   // Copy constructor
+    Optional(const Optional& copy): has_value_(copy.has_value_)
+    {
+        new(data_)T(*reinterpret_cast<const T*>(copy.data_));
+    }
+
+    //Move constructor
+    Optional(Optional&& copy): has_value_(copy.has_value_)
+    {
+        new(data_)T(std::move(*reinterpret_cast<T*>(copy.data_)));
+        copy.has_value_ = false;
+        memset(copy.data_, 0, sizeof(T));
+    }
+
+    //Move Assignment Operator
+    Optional& operator=(Optional&& copy)
+    {
+        has_value_ = copy.has_value_;
+        new(data_)T(std::move(*reinterpret_cast<T*>(copy.data_)));
+        copy.has_value_ = false;
+        memset(copy.data_, 0, sizeof(T));
+       return *this;
+    }
+
+    bool has_value()
+    {
+        return has_value_;
+    }
+
+    T& value()
+    {
+        T* temp = reinterpret_cast<T*>(data_);
+        return *temp;
+    }
+
+    //Copy Assignment Operator
+    Optional& operator=(Optional& copy)
+    {
+        std::cout << "Copy Assignment Operator" << std::endl;
+        has_value_ = copy.has_value_;
+        new(data_)T(*(reinterpret_cast<T*>(copy.data_)));
+        return *this;
+    }
+
+    ~Optional()
+    {
+
+    }
 
     private:
-    T* m_ptr;
+    bool has_value_;
+    char data_[sizeof(T)];
 
-}
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,56 @@
-#include<iostream>
+#include <Optional.hpp>
+#include <cassert>
+#include <iostream>
 
+void testWithStruct() {
+    struct S {
+        S(int a, int b, int c) : m_a(a), m_b(b), m_c(c) {}
+        ~S() {}
+        int m_a;
+        int m_b;
+        int m_c;
+    };
 
-int main()
-{
-    std::cout << "here";
+    S           s(1, 2, 3);
+    Optional<S> temp1(s); // Constructor
+    assert(temp1.has_value() == true);
+    assert(temp1.value().m_a == 1);
+    assert(temp1.value().m_b == 2);
+    assert(temp1.value().m_c == 3);
+    Optional<S> temp2 = temp1;
+    assert(temp2.has_value() == true);
+    assert(temp2.value().m_a == 1);
+    assert(temp2.value().m_b == 2);
+    assert(temp2.value().m_c == 3);
+}
+
+// Copy Constructor
+void testAllBasic() {
+    Optional<int> temp = 2; // param constructor
+    assert(temp.has_value() == true);
+    assert(temp.value() == 2);
+    Optional<int> temp2(temp); // copy constructor check
+    assert(temp2.has_value() == true);
+    assert(temp2.value() == 2);
+    Optional<int> temp3;
+    assert(temp3.has_value() == false);
+    temp3 = temp; // Copy Assignment operator
+    assert(temp3.has_value() == true);
+    assert(temp3.value() == 2);
+    Optional<int> temp4(std::move(temp)); // Move constructor
+    assert(temp4.has_value() == true);
+    assert(temp4.value() == 2);
+    assert(temp.has_value() == false);
+    assert(temp.value() == 0);
+    temp = std::move(temp4); // Move Assignment operator
+    assert(temp.has_value() == true);
+    assert(temp.value() == 2);
+    assert(temp4.has_value() == false);
+    assert(temp4.value() == 0);
+}
+
+int main() {
+    //  testAllBasic();
+    testWithStruct();
+    return 0;
 }
